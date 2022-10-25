d9b084f567f75a9a62985e06cc10fbff9dd31a87
flavio19744791 <113073146+flavio19744791@users.noreply.github.com>
2022-10-25 00:20:01 +0000

    tarefafa 1 estrutura og

---
+++ app/controllers/subscripts_controller.rb
@@ -0,0 +1,5 @@
+class SubscriptsController < ApplicationController
+def new
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
    root "home#index"
+   resources :subscripts, only:  [:new]
 end
